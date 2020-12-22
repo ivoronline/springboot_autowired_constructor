ba973062596aaea01995f49cdafbb1f125256f42
igoreta <ivoronline@gmail.com>
2020-12-22 16:24:40 +0100

    Initial commit

---
+++ src/main/java/com/ivoronline/springboot_autowired_constructor/SpringbootAutowiredConstructorApplication.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_autowired_constructor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAutowiredConstructorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootAutowiredConstructorApplication.class, args);
+  }
+
+}
